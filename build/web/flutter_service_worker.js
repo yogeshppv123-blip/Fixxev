4bee6b6c98d0e20d4586d47f53f600abb60e06c4
yogeshppv123-blip <yogeshppv123@gmail.com>
2026-01-08 18:55:45 +0530

    Update frontend with logo sizing and upload fixes

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3636bf4b8fe55ec072dc8ba2a46b73a2",
+const RESOURCES = {"flutter_bootstrap.js": "ea0e07f6a46573e55d89633c5456c10b",
 "version.json": "07adb36add9c276c092acf9b5c2a8d14",
 "index.html": "6e5461294a6860f59d6cfbd18b62c8fe",
 "/": "6e5461294a6860f59d6cfbd18b62c8fe",
-"main.dart.js": "48d9981e194f8b2e39d4ba02504b77eb",
+"main.dart.js": "9ee7909e1c172be226c1dd85bdb11080",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "logo.png": "d844100f2d49dbc597818c4fd98c9330",
